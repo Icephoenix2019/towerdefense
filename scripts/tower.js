0363e8c35844f39deb3fb7d1be9b0a64559d8d36
Icephenoix2019 <56941962+Icephenoix2019@users.noreply.github.com>
2021-09-08 19:53:08 -0400

    Update tower.js

--- scripts/tower.js
+++ scripts/tower.js
@@ -83,7 +83,7 @@ class Tower {
     // Draw base of tower (stationary part)
     drawBase() {
         if (imagelib.hasOwnProperty(this.imageBase)) {
-            image(imagelib[this.imageBase], this.pos.x, this.pos.y);
+            image(imagelib[this.imageBase], this.pos.x, this.pos.y, this.radius * ts, this.radius * ts);
         } else {
             stroke(this.border);
             fill(this.color);
